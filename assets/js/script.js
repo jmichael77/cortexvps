7f439fbd3480d3faf3ff3a956ff23d763a62cf24
jmichael77 <jmichaely2k7@gmail.com>
2017-08-16 16:56:27 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    //Features appearance
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        // >=, not <=
+        if (scroll >= 450) {
+            $(".feature-icon").addClass("feature-display");
+            $(".feature-head-text").addClass("feature-display");
+            $(".feature-subtext").addClass("feature-display");
+        }
+    });
+
+//smooth scrolling
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+})
